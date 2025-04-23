f514bfbd61e23939fcf3a410e560b800c63e13b6
Boomchain Labs <202796817+BoomchainLabs@users.noreply.github.com>
2025-04-23 23:17:48 +0100

    Create App.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+import Logo from './components/Logo';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Logo />
+    </div>
+  );
+};
+
+export default App;
